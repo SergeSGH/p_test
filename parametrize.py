00bba5b52328d9780cac9557eae264cab72f782c
Sergey <ssprihodko@yandex.ru>
2022-06-05 15:28:49 +0300

    1st part

---
+++ parametrize.py
@@ -0,0 +1,12 @@
+import pytest
+
+def idfn_x(val):
+    return "x=({0})".format(str(val))
+    
+def idfn_y(val):
+    return "y=({0})".format(str(val))
+
+@pytest.mark.parametrize("x", [1,2], ids=idfn_x)
+@pytest.mark.parametrize("y", [10,11], ids=idfn_y)
+def test_cross_params(x, y):
+    print("x: {0}, y: {1}".format(x, y))
